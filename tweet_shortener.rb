7ab7d465742d944ddd63385b9b443b4fe4d529c7
Jeffrey T Gosselin <jgoz.email@gmail.com>
2018-11-27 03:45:56 +0000

    automatically backed up by learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -33,7 +33,7 @@ def bulk_tweet_shortener(tweets)
 end
 
 def selective_tweet_shortener(tweet)
-  if tweet.join(" ").length > 140 
+  if tweet.length > 140 
     word_substituter(tweet)
   else
     tweet
